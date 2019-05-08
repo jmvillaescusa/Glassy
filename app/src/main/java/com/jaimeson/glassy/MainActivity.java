3d906525db08022dc5f77c94a3bf95f27f748b6b
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-05-07 21:04:40 -0400

    App Created

--- app/src/main/java/com/jaimeson/glassy/MainActivity.java
+++ app/src/main/java/com/jaimeson/glassy/MainActivity.java
@@ -1,10 +1,12 @@
-package com.jaimeson.shatteredart;
+package com.jaimeson.glassy;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -13,7 +15,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Buttons
+        Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+        Button buttonQuit = (Button)findViewById(R.id.buttonQuit);
     }
 
     @Override
