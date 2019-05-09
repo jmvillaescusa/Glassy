c8a674ecaace5037ad237a3aea3969b5bcf8ad95
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-05-08 21:05:39 -0400

    Update MainActivity.java

--- app/src/main/java/com/jaimeson/glassy/MainActivity.java
+++ app/src/main/java/com/jaimeson/glassy/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //Exit Game
                 finish();
                 System.exit(0);
-                //onDestroy();
                 break;
             case R.id.buttonHiScore:
                 //Display top scores
